acf8b7ed9a5902b6b99338b3cdcadccac1b45b2c
Esraa <esraayaareb@gmail.com>
2021-09-30 17:21:19 +0300

    add sort feature

--- task.js
+++ task.js
@@ -1,44 +1,40 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
+// change the numbers array from let to const because the original numbers array should be immutable const numbers = [5, 8, 0, 1, 9, 11, 15, 16]
+// use functions to sort the numbers to prevent repeated code (DRY Code) and to return a new array instead of changing the original one and to make the code more reusable
+// i < numbers.length - 1 instead of i < numbers.length because we don't compare the last number in the array with another number
+// j < numbers.length - 1 - i instead of j < numbers.length - 1 because for every iteration the biggest number is already sorted
+// numbers[j] > numbers[j+1] instead of numbers[i] > numbers[j + 1] because the inner loop is for j so i only changes after the inner loop finishes
+// [numbers[j], numbers[j + 1]] = [numbers[j + 1], numbers[j]] instead of the temp method to have shorter and easier code
 
-console.log("Original numbers list: ", numbers)
+const numbers = [5, 8, 0, 1, 9, 11, 15, 16];
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] > numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
+const ascSort = (originalNumbers) => {
+  let numbers = [...originalNumbers];
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = 0; j < numbers.length - 1 - i; j++) {
+      if (numbers[j] > numbers[j + 1]) {
+        [numbers[j], numbers[j + 1]] = [numbers[j + 1], numbers[j]];
+      }
     }
-}
+  }
+  return numbers;
+};
 
-console.log("Numbers list After sorting: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] < numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
+const descSort = (originalNumbers) => {
+  let numbers = [...originalNumbers];
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = 0; j < numbers.length - 1 - i; j++) {
+      if (numbers[j] < numbers[j + 1]) {
+        [numbers[j], numbers[j + 1]] = [numbers[j + 1], numbers[j]];
+      }
     }
-}
+  }
+  return numbers;
+};
 
-console.log("Numbers list After Desc sorting: ", numbers)
+const ascendingNumbers = ascSort(numbers);
+console.log('Numbers list After Asc sorting: ', ascendingNumbers);
 
+const descendingNumbers = descSort(numbers);
+console.log('Numbers list After Desc sorting: ', descendingNumbers);
 
+console.log('Original numbers list: ', numbers);
